272ab86757ed3476d22c4f8513e0e5a050f4f4b4
HSYN <huseynmirzayev8@gmail.com>
2023-07-21 18:24:09 +0400

    adding add todo functionality

--- 6. To-do List/script.js
+++ 6. To-do List/script.js
@@ -7,3 +7,17 @@ const taskList = document.querySelector(".taskList ul");
 const addInput = document.getElementById("input");
 
 let todos = [];
+
+// Adding todo functionality
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const taskText = addInput.value.trim();
+
+  if (!taskText) return;
+
+  todos.push(taskText);
+  console.log(todos);
+  addInput.value = "";
+});
