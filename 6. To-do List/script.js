94881c9703bbfc3e37f34dbc4b5dd38578209ec5
HSYN <huseynmirzayev8@gmail.com>
2023-07-21 12:05:52 +0400

    add element variables to script on todo project

---
+++ 6. To-do List/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const addBtn = document.getElementById("add");
+
+const taskListContainer = document.querySelector(".taskList");
+const taskList = document.querySelector(".taskList ul");
+const addInput = document.getElementById("input");
+
+let todos = [];
