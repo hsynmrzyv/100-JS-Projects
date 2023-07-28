24b742b74222c21f2bd857712b7ef816a270c1f4
HSYN <huseynmirzayev8@gmail.com>
2023-07-28 12:51:15 +0400

    add calculator

---
+++ 8. Calculator/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const btns = document.querySelectorAll("#calculator__buttons button");
+const calculatorDisplay = document.querySelector("#calculator__display h1");
+const clearBtn = document.getElementById("clear__btn");
+
+// Initial values
+let firstValue = 0;
+let operatorValue = "";
+let waitingNextValue = false;
+
+const sendNumberValue = (number) => {};
+
+const addDecimal = () => {};
+
+const resetAll = () => {};
+
+const useOperator = (operator) => {};
