07b9e54f91638add0eb840081cc5f2ef08616c1e
HSYN <huseynmirzayev8@gmail.com>
2023-08-02 21:25:35 +0400

    add clock boilerplaye

---
+++ 14. Clock/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const analogSecond = document.getElementById("second");
+const analogMinute = document.getElementById("minute");
+const analogHour = document.getElementById("hour");
+
+const digitalSecond = document.getElementById("d__second");
+const digitalMinute = document.getElementById("d__minute");
+const digitalHour = document.getElementById("d__hour");
