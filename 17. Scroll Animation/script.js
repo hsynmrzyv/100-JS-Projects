5693f1936baa9eb43efb3a24e22d69b640e917e0
Huseyn Mirzayev <70962188+hsynmrzyv@users.noreply.github.com>
2024-06-28 14:01:38 +0400

    Update script.js

--- 17. Scroll Animation/script.js
+++ 17. Scroll Animation/script.js
@@ -2,27 +2,3 @@
 
 const cards = document.querySelectorAll(".card");
 
-cards.forEach((card, index) => {
-  card.style.transform =
-    index % 2 === 0 ? `translateX(50%)` : `translateX(-50%)`;
-  card.style.opacity = "0";
-});
-
-const scrollAnimation = () => {
-  let triggerPoint = window.innerHeight * 0.8;
-  cards.forEach((card, index) => {
-    const cardTopDistance = card.getBoundingClientRect().top;
-
-    if (cardTopDistance < triggerPoint) {
-      card.style.transform = `translateY(0%)`;
-      card.style.opacity = "1";
-    } else {
-      card.style.transform =
-        index % 2 === 0 ? `translateX(50%)` : `translateX(-50%)`;
-      card.style.opacity = "0";
-    }
-  });
-};
-
-scrollAnimation();
-window.addEventListener("scroll", scrollAnimation);
